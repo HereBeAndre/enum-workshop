24b7f3654bb2920d3c2a061502b80bcab940ad1f
HereBeAndre <andrea.tota6@gmail.com>
2024-02-26 17:06:26 +0100

    Add enum as argument example

---
+++ src/rust/enum_as_arg.rs
@@ -0,0 +1,22 @@
+enum Color {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+}
+
+fn print_color(color: Color) {
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Yellow => println!("yellow"),
+    }}
+
+fn main () {
+    let red = Color::Red;
+    let _green = Color::Green;
+    let _yellow = Color::Yellow;
+    print_color(red);
+}
+
+// rustc src/rust/enum_as_arg.rs
